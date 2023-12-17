ef4ad2d9a8de636e955d0b457aef7192c9bba62b
Veerle van Leemput <info@hypebright.nl>
2023-12-17 13:46:15 +0100

    use devicePage module in main.R

--- app/main.R
+++ app/main.R
@@ -3,19 +3,13 @@ box::use(
   shiny[bootstrapPage, div, moduleServer, NS, renderUI, tags, uiOutput, icon, tagList, br,
         reactiveValues, p],
   shinyjs[useShinyjs],
-  shinytoastr[useToastr],
-  waiter[useWaiter, waiterPreloader, spin_6]
+  shinytoastr[useToastr]
 )
 
 box::use(
   app/logic/constants,
   app/logic/functions,
-  app/view/dataUpload,
-  app/view/calendar,
-  app/view/visualization,
-  app/view/analysis,
-  app/view/cutData,
-  app/view/batch
+  app/view/devicePage
 )
 
 #' @export
@@ -25,126 +19,14 @@ ui <- function(id) {
     header = tagList(
       useShinyjs(),
       useToastr(),
-      # useWaiter(),
-      # waiterPreloader(
-      #   html = tagList(spin_6(),
-      #                  br(),
-      #                  br(),
-      #                  "Loading..."
-      #   )
-      # )
     ),
     id = ns("navbar_id"),
     title = "Wearalyze",
     theme = constants$wearalyze_theme,
     fillable = FALSE,
-    nav_menu(
-      title = "E4",
-      value = "e4-menu",
-      icon = icon("heart-circle-bolt"),
-      nav_panel("Data",
-                icon = icon("file-upload"),
-                dataUpload$ui(ns("e4-data"))
-      ),
-      nav_panel("Calendar",
-                icon = icon("calendar-alt"),
-                calendar$ui(ns("e4-calendar"))
-      ),
-      nav_panel("Visualization",
-                icon = icon("chart-bar"),
-                visualization$ui(ns("e4-visualization"))
-      ),
-      nav_panel("Analysis",
-                icon = icon("chart-line"),
-                analysis$ui(ns("e4-analysis"))
-      ),
-      nav_panel("Data cutter",
-                icon = icon("cut"),
-                cutData$ui(ns("e4-cut"))
-      ),
-      nav_panel("Batch analysis",
-                icon = icon("list-ol"),
-                batch$ui(ns("e4-batch"))
-      )
-    ),
-    nav_menu(
-      title = "Embrace Plus",
-      value = "embrace-plus-menu",
-      icon = icon("heart-pulse"),
-      nav_panel("Data",
-                icon = icon("file-upload"),
-                card(
-                  p("placeholder")
-                )
-                
-      ),
-      nav_panel("Calendar",
-                icon = icon("calendar-alt"),
-                card(
-                  p("placeholder")
-                )
-                
-      ),
-      nav_panel("Visualization",
-                icon = icon("chart-bar"),
-                card(
-                  p("placeholder")
-                )
-                
-      ),
-      nav_panel("Analysis",
-                icon = icon("chart-line"),
-                card(
-                  p("placeholder")
-                )
-      ),
-      nav_panel("Batch analysis",
-                icon = icon("list-ol"),
-                card(
-                  p("placeholder")
-                )
-      )
-    ),
-    nav_menu(
-      title = "Nowatch",
-      value = "nowatch-menu",
-      icon = icon("clock"),
-      nav_panel("Data",
-                icon = icon("file-upload"),
-                card(
-                  p("placeholder")
-                )
-                
-      ),
-      nav_panel("Calendar",
-                icon = icon("calendar-alt"),
-                card(
-                  p("placeholder")
-                )
-                
-      ),
-      nav_panel("Visualization",
-                icon = icon("chart-bar"),
-                card(
-                  p("placeholder")
-                )
-                
-      ),
-      nav_panel("Analysis",
-                icon = icon("chart-line"),
-                card(
-                  p("placeholder")
-                )
-                
-      ),
-      nav_panel("Batch analysis",
-                icon = icon("list-ol"),
-                card(
-                  p("placeholder")
-                )
-                
-      )
-    )
+    devicePage$ui(id = ns("e4"), device = "E4"),
+    devicePage$ui(id = ns("embrace-plus"), device = "Embrace Plus"),
+    devicePage$ui(id = ns("nowatch"), device = "Nowatch"),
   )
 }
 
@@ -161,25 +43,9 @@ server <- function(id) {
     }
     
     # Modules ---------------------------------------
-    ## E4 -------------------------------------------
-    e4_data_in <- dataUpload$server(id = "e4-data", 
-                                    device = "e4")
-    
-    e4_calendar <- calendar$server(id = "e4-calendar")
-    
-    e4_visualization <- visualization$server(id = "e4-visualization",
-                                             data = e4_data_in,
-                                             device = "e4")
-    
-    analysis$server(id = "e4-analysis",
-                    data = e4_data_in,
-                    plots = e4_visualization,
-                    calendar = e4_calendar)
-    
-    cutData$server(id = "e4-cut",
-                   data = e4_data_in)
-    
-    batch$server(id = "e4-batch")
+    devicePage$server(id = "e4", device = "E4")
+    devicePage$server(id = "embrace-plus", device = "Embrace Plus")
+    devicePage$server(id = "nowatch", device = "Nowatch")
     
   })
 }
