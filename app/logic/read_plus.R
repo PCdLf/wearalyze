94baca8c427b9e71ead13eac818a927de71ab06a
Veerle van Leemput <info@hypebright.nl>
2024-01-12 13:43:00 +0100

    refactor read_plus function

--- app/logic/read_plus.R
+++ app/logic/read_plus.R
@@ -1,7 +1,14 @@
-library(reticulate)
-use_python("C:\\ProgramData\\anaconda3\\python.exe", required = TRUE)
 
-source_python("C:\\Users\\acans\\Desktop\\read_plus.py")
+box::use(
+  reticulate[source_python]
+)
 
-root="C:\\Users\\acans\\Desktop\\2023-07-11.zip"
-avro_files <- read_e4_plus(root)
+read_plus <- function(path) {
+  
+  reticulate::source_python("./app/logic/read_plus.py")
+  
+  avro_files <- read_e4_plus(path)
+  
+  return(avro_files)
+  
+}
